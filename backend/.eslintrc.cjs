7a8bf829bc4b32c8064b25e96dc9d6a7ea7220b6
godack <isaiasvelasotil@gmail.com>
2026-02-23 12:13:29 +0100

    Fix: CI Workflow (linting process)

---
+++ backend/.eslintrc.cjs
@@ -0,0 +1,15 @@
+module.exports = {
+  env: {
+    node: true,
+    es2020: true,
+  },
+  extends: ["eslint:recommended"],
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: "module",
+  },
+  ignorePatterns: ["dist"],
+  rules: {
+    "no-unused-vars": ["warn", { argsIgnorePattern: "^_" }],
+  },
+};
---
+++ frontend/.eslintrc.cjs
@@ -0,0 +1,14 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2020: true,
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:react-hooks/recommended",
+  ],
+  parser: "@typescript-eslint/parser",
+  plugins: ["@typescript-eslint", "react-hooks"],
+  ignorePatterns: ["dist"],
+};
